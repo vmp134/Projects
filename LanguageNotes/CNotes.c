4d588de18c91f6bddc22b6a9b03fee9d04a4cc8b
Victor Peng <vmp134@scarletmail.rutgers.edu>
2025-07-02 15:35:59 -0400

    Added new notes

---
+++ LanguageNotes/CNotes.c
@@ -0,0 +1,18 @@
+// This is a comment.
+
+/*
+This is a multi-line comment.
+*/
+
+// Like Java, you must declare variable types and use semicolons.
+int i = 10;
+char *last = "Hello World!";
+
+//Unlike Java, Strings don't really "exist" in C, you are forced to use char pointers.
+printf("%s", last);
+
+//On the topic of pointers, think of them as pretty much the same as arrays in Java.
+
+
+
+
