c9b04b09f9ad57bab62c2299ff158871f44b17b8
Victor <victorpeng06@gmail.com>
2025-10-22 00:53:35 -0400

    Some stuff

---
+++ Example Projects/30DaysOfPython/day_9/Conditionals.py
@@ -0,0 +1,32 @@
+#1
+print("Enter your age: ")
+age = str(input())
+if age >= 18:
+    print("You are old enough to learn to drive.")
+else:
+    print("You need " + 18-age + " more years to learn to drive.")
+
+#2
+print("Enter your age: ")
+age = str(input())
+if age == 19:
+    print("You are the same age as me.")
+elif age == 18 or age == 20:
+    if age == 18: print("You are one year younger than me.")
+    else: print("You are one year older than me.")
+elif age < 18: print ("You are " + 19-age + " years younger than me.")
+else: print ("You are " + age-19 + " years older than me.")
+
+#3
+
+
+#1
+
+
+#2
+
+
+#3
+
+
+#1
