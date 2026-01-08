aeee87bc3e42bcbdbbb4d68a6415a1ee1b3e747c
Victor <victorpeng06@gmail.com>
2026-01-07 21:25:29 -0800

    Some changes

---
+++ Self-Made Projects/Notes/30DaysOfPythonNotes.py
@@ -0,0 +1,88 @@
+#Day 1 - Introduction
+  
+
+#Day 2 - Variables, Built-in Functions
+
+
+#Day 3 - Operators
+
+
+#Day 4 - Strings
+
+
+#Day 5 - Lists
+
+
+#Day 6 - Tuples
+
+
+#Day 7 - Sets
+
+
+#Day 8 - Dictionaries
+
+
+#Day 9 - Conditionals
+
+
+#Day 10 - Loops
+
+
+#Day 11 - Functions
+
+
+#Day 12 - Modules
+
+
+#Day 13 - List Comprehension
+
+
+#Day 14 - Higher Order Functions
+
+
+#Day 15 - Python Type Errors
+
+
+#Day 16 - Python Date Time
+
+
+#Day 17 - Exception Handling
+
+
+#Day 18 - Regular Expressions
+
+
+#Day 19 - File Handling
+
+
+#Day 20 - Python Package Manager
+
+
+#Day 21 - Classses and Objects
+
+
+#Day 22 - Web Scraping
+
+
+#Day 23 - Virtual Environment
+
+
+#Day 24 - Statistics
+
+
+#Day 25 - Pandas
+
+
+#Day 26 - Python Web
+
+
+#Day 27 - Python with MongoDB
+
+
+#Day 28 - API
+
+
+#Day 29 - Building API
+
+
+#Day 30 - Conclusions
