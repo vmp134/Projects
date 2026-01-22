d2941ad0596febac0c5922b8ba3343b93707d981
Victor <victorpeng06@gmail.com>
2026-01-21 20:30:24 -0500

    Some changes

--- Self-Made Projects/Notes/CNotes.c
+++ Self-Made Projects/Notes/CNotes.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
 // This is a comment.
 
@@ -9,7 +10,7 @@ This is a multi-line comment.
 */
 
 //Like Java, you must have a main method.
-void main(void) {
+void main(int argc, char **argv) {
 
     // Like Java, you must declare variable types and use semicolons.
     uint8_t i = 10;
@@ -47,6 +48,8 @@ void main(void) {
     
     free(arr);
     free(matrix);
+
+    return EXIT_SUCCESS;
 }
 
 
